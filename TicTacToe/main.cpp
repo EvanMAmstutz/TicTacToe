5a31d493e596f57666e32809d4f8507bbc249f37
Evan Amstutz <evanamstutz@Evans-MacBook-Air.local>
2015-03-23 16:27:39 -0400

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Evan Amstutz on 3/23/15.
+//  Copyright (c) 2015 Evan Amstutz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
